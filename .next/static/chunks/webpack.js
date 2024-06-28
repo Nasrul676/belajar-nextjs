b02b4464d072fdce2dc0dace982b67bee0a1382e
Nasrul676 <nasrulmuhammad676@gmail.com>
2024-06-28 10:08:50 +0700

    fix bug font

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("906468562b1da501")
+/******/ 		__webpack_require__.h = () => ("0ac76cadaf31b8e1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
